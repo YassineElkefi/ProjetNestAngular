1567e46f0f67f3b1ffac883d29ec1adefa33a426
Hiba-Bahri <hibabahri447@gmail.com>
2024-05-19 14:50:29 +0100

    Database connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as mongoose from 'mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING)],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit{
+
+  onModuleInit() {
+    const db = mongoose.connection;
+    db.on('connected', () => {
+      console.log('Mongoose connected');
+    });
+
+    db.on('error', (error) => {
+      console.error('Mongoose connection error:', error);
+    });
+  }
+
+}
