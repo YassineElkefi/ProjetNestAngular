f28f95afc07b843ceecc0db10b8b57dd68f600ba
Hiba-Bahri <hibabahri447@gmail.com>
2024-05-21 12:32:19 +0100

    Generation des rapports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { DepensesComponent } from './depenses/depenses.component';
 import { FormsModule } from '@angular/forms';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { DepenseChartComponent } from './depense-chart/depense-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DepensesComponent,
+    DepenseChartComponent,
   ],
   imports: [
     BrowserModule,
